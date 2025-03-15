0929915ff776957ec146d069e299478b80b53268
Nour Al Safadi <nour.safadi7@icloud.com>
2025-03-14 20:07:57 -0400

    creates interval function and displays satellites coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  {{ data_information }}
-  <api_app/>
+  <div> {{ message }}</div>
+  <!-- <div> {{ coordinates_sat }}</div> -->
+  <div v-for="sat in coordinates_sat"
+  :key="sat"
+  >
+    <div v-for="coord in sat" :key="coord">
+      {{ coord }}
+    </div>
+  </div>
 </template>
 
 <script>
-import api_app from './components/api_app.vue'
+// import api_app from './components/api_app.vue'
 
 const API_KEY = process.env.VUE_APP_API;
 const API_URL = `https://api.spectator.earth/satellite/?api_key=${API_KEY}`;
@@ -13,25 +20,57 @@ const API_URL = `https://api.spectator.earth/satellite/?api_key=${API_KEY}`;
 
 export default {
   name: 'App',
-  components: {
-    api_app
-  },
+  // components: {
+  //   api_app
+  // },
   data(){
     return{
-      sat_data: null
+      sat_data: null, 
+      interval: null,
+      message: '',
+      coordinates_sat: '',
+      test_interval: null,
+      sat_cords:[]
     }
   },
   computed: {
-    data_information(){
-     return console.log("MY DATA",this.sat_data)
-    }
+    // data_information(){
+    //  return console.log("MY DATA",this.sat_data)
+    // }
   },
   mounted(){
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) =>{
+    this.fetch_data();
+    this.interval = setInterval(()=>{
+      this.coordinates_sat = this.sat_data.features.map(d => d.geometry.coordinates)
+      console.log(this.coordinates_sat)
+      console.log("Fetching data");
+      this.fetch_data();
+      }, 
+      2000);
+    
+    this.test_interval = setInterval(() => {
+      this.message = new Date().toLocaleTimeString();
+    }, 1000)
+  },
+  methods:{
+    async fetch_data(){
+      try{
+        const res = await fetch(API_URL, {cache: "no-store"});
+        if (!res.ok) throw new Error (`AIP Error: ${res.status}`)
+        const data = await res.json();
         this.sat_data = data;
-      });
+        console.log("Updated Data", data)
+      } catch (error) {
+        console.error("Fetch error:", error)
+      }
+        
+    }
+  }, 
+  beforeUnmount(){
+    clearInterval(this.interval);
+    console.log("Interval Cleared");
+    clearInterval(this.test_interval);
+    console.log("interval cleared tested")
   }
 };
 </script>
