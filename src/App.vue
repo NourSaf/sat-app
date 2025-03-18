f1bf1dfad9bcdc6460ab99e416001bb781832021
Nour Al Safadi <nour.safadi7@icloud.com>
2025-03-18 17:49:47 -0400

    sets up GeminiResponses component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <h1>The Dreaming Sat</h1>
-    <h3>Current Locations:</h3>
-    <p> {{ coordinates_sat }}</p>
-    <h3>Dream:</h3>
-    <p>{{ geminiResponse }}</p>
+    <h3> Current Locations: </h3>
+    
+    <p> {{ coordinates_sat }} </p>
+    
+    <div class="section">
+      <h3>GREAM</h3>
+      <GeminiResponse :response="responses"/>
+    </div>
+
   </div>
 </template>
 
 <script>
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import GeminiResponse from "./components/GeminiResponse.vue"
 
 const API_KEY = process.env.VUE_APP_API;
 const API_URL = `https://api.spectator.earth/satellite/?api_key=${API_KEY}`;
@@ -19,18 +25,23 @@ const genAI = new GoogleGenerativeAI("AIzaSyCZjHuMPr9JPA37ps8Gs1z3xgN9x3LGVB0");
 //https://visualalchemist.in/2024/06/25/data-driven-art-inspiring-creativity-with-real-world-data/
 
 export default {
-  name: 'apiData',
-      data(){
-    return{
-      sat_data: null, 
-      interval: null,
-      message: '',
-      coordinates_sat: '',
-      display_time: null,
-      sat_cords:[], 
-      geminiResponse: '',
-      prompt: ''
+  name: 'App',
+    data() {
+      return {
+        sat_data: null, 
+        interval: null,
+        message: '',
+        coordinates_sat: '',
+        display_time: null,
+        sat_cords:[], 
+        gemini_response: '',
+        prompt: '',
+        responses:[]
+
     }
+  },
+  components:{
+    GeminiResponse
   },
   computed: {
     // data_information(){
@@ -55,10 +66,11 @@ export default {
   coordinates_sat: {
     deep: true,
     handler(newCoords) {
-      if (newCoords[0] && newCoords[1] && newCoords[2]) {
-        this.prompt = `Generate the satellites dream based of what the satellite is seeing from lattitude ${newCoords[0]}, longitude ${newCoords[1]} and altitude ${newCoords[3]}. Generate only the dream as a poem.`;
+        if (newCoords[0] && newCoords[1] && newCoords[2]) {
+        this.prompt = `Generate the satellites dream based of what the satellite is seeing from lattitude ${newCoords[0]}, longitude ${newCoords[1]} and altitude ${newCoords[2]}. Generate only the dream as a poem.`;
         this.sendToGemini();
       }
+      
     },
   },
 },
@@ -77,12 +89,11 @@ export default {
 
     async sendToGemini(){
       try{
-        
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
         const result = await model.generateContent(this.prompt);
         const dream = result.response.text()
         // console.log(result.response.text());
-        this.geminiResponse = dream;
+        this.gemini_response = dream;
       }catch (error) {
         console.error("Error fetching response from Gemini:", error);
       }
---
+++ src/components/GeminiResponse.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        v-for="key in responses"
+        :key="key"
+        class="pormpts">  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GeminiResponse',
+    data() {
+        return{
+
+        }
+    },
+    props: {
+        responses: Array
+    },
+    methods:{
+
+    }
+};
+</script>
